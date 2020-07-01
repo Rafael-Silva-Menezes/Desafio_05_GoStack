0420abddb0affab6dac3651881dd0b360e0d168b
rafasilva <rafasilva320.rs@gmail.com>
2020-07-01 08:23:13 -0300

    Desafio com todos os testes aprovados

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Transaction is invalid');
+
+    }
+
+    if (type === 'outcome' && total < value) {
+      throw Error('Impossible to withdraw, the amount is greater than the total');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    })
+
+    return transaction
   }
 }
 
